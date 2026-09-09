a34dd4a986a1a6ca084c6e40e74db0bb087ffa62
Shpro123456789 <107074467+Shoro123456789@users.noreply.github.com>
2026-09-09 13:00:18 +0100

    collisions for everyone and a delta time slider

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import math
 from direct.task import Task
 from direct.showbase.ShowBaseGlobal import globalClock
 from random import randint
+import simplepbr
+from direct.gui.DirectGui import *
+
 G=6.67*(10**(-11))
+
 #class Sphere:
  #   def __init__(self, scale, colour, position):
 #        self.model = ShowBase.loader.loadModel("models/misc/sphere")
@@ -18,6 +22,7 @@ class Body:
     def __init__(self, loader, render, mass, scale, colour, position, velocity=None, acceleration=None):
         self.model = loader.loadModel("models/misc/sphere")
         self.model.reparentTo(render)
+        self.scale=scale
         self.model.setScale(scale)
         self.model.setPos(position)
         self.model.setColor(*colour)
@@ -31,9 +36,15 @@ class Body:
         
     def setPos(self, position):
         self.model.setPos(position)
+
+    def distance_to(self, other):
+        diff = other.getPos() - self.getPos()
+        r = diff.length()
+        return diff, r
+
     def force(self, other):
         diff=other.getPos()-self.getPos()
-        r= diff.length()
+        r= self.distance_to(other)[1]
         if r == 0:
             return Vec3(0,0,0)
         force=(G)*self.mass*other.mass/(r*r)
@@ -43,52 +54,101 @@ class Body:
         self.acceleration = force/self.mass
         self.velocity += self.acceleration * dt
         self.setPos(self.getPos() + self.velocity * dt)
+    def is_colliding(self, other):
+        if self.distance_to(other)[1] < self.scale + other.scale:
+#            print("collision")
+            return True
+        else:
+#            print("no collision")
+            return False
+    def apply_collision(self, other):
+        normal = (other.getPos() - self.getPos()).normalized()
+
+        self_normal_speed = self.velocity.dot(normal)
+        self_tangential = self.velocity - normal * self_normal_speed
+
+        other_normal_speed = other.velocity.dot(normal)
+        other_tangential = other.velocity - normal * other_normal_speed
+
+        new_self_normal_speed = ((self.mass - other.mass) * self_normal_speed
+                                + 2 * other.mass * other_normal_speed) / (self.mass + other.mass)
+        new_other_normal_speed = ((other.mass - self.mass) * other_normal_speed
+                                + 2 * self.mass * self_normal_speed) / (self.mass + other.mass)
+
+        new_self_velocity = normal * new_self_normal_speed + self_tangential
+        new_other_velocity = normal * new_other_normal_speed + other_tangential
+
+        self.velocity = new_self_velocity
+        other.velocity = new_other_velocity
 
 
 class MyApp(ShowBase):
 
     def __init__(self):
-        
         ShowBase.__init__(self)
-#        self.model = self.loader.loadModel("models/misc/sphere")
-#        self.model.reparentTo(self.render)
-#        self.model.setScale(2)
-#        self.model.setPos(0, 10, 0)
-#        self.geom = self.model.find("**/+GeomNode").node()
- #       self.model.setColor(0.6, 0.6, 1.0, 1.0)
-#        self.sphere = Sphere(2, (0.6, 0.6, 1.0, 1.0), (0, 10, 0))
-#        self.sphere.reparentTo(self.render)
-#        self.sphere = self.loader.loadModel("models/misc/sphere")
-#        self.sphere.reparentTo(self.render)
-#        self.sphere.setScale(2)
-#        self.sphere.setPos(10, 10, 10)
-#        self.sphere.setColor(0.1, 0.8, 1.0, 1.0)
-#        self.mass1=100000000000.0
-#        self.mass2=100000000000.0
-#        self.velocity=Vec3(0,0,0)  
-#        self.acceleration=Vec3(0,0,0)      
+
+
+        # simplepbr.init(enable_shadows=True)
+        # directional = DirectionalLight("directional")
+        # directional.setColor((4.0, 3.8, 3.4, 1))
+        # directional.setShadowCaster(True, 2048, 2048)  
+        # directional_np = self.render.attachNewNode(directional)
+        # directional_np.setHpr(45, -60, 0)
+        # self.render.setLight(directional_np)
+        # ambient = AmbientLight("ambient")
+        # ambient.setColor((0.15, 0.15, 0.18, 1))  # dim, slightly cool fill
+        # ambient_np = self.render.attachNewNode(ambient)
+        # self.render.setLight(ambient_np)
+                
+    #    self.model = self.loader.loadModel("models/misc/sphere")
+    #    self.model.reparentTo(self.render)
+    #    self.model.setScale(2)
+    #    self.model.setPos(0, 10, 0)
+    #    self.geom = self.model.find("**/+GeomNode").node()
+    #    self.model.setColor(0.6, 0.6, 1.0, 1.0)
+    #    self.sphere = Sphere(2, (0.6, 0.6, 1.0, 1.0), (0, 10, 0))
+    #    self.sphere.reparentTo(self.render)
+    #    self.sphere = self.loader.loadModel("models/misc/sphere")
+    #    self.sphere.reparentTo(self.render)
+    #    self.sphere.setScale(2)
+    #    self.sphere.setPos(10, 10, 10)
+    #    self.sphere.setColor(0.1, 0.8, 1.0, 1.0)
+    #    self.mass1=100000000000.0
+    #    self.mass2=100000000000.0
+    #    self.velocity=Vec3(0,0,0)  
+    #    self.acceleration=Vec3(0,0,0) 
+        self.slider = DirectSlider(range=(0,5), value=1, pageSize=1,
+    pos=(-0.8, 0, 0.8),
+    scale=0.3,
+    frameColor=(0.15, 0.15, 0.18, 0.9),   # dark translucent track
+    thumb_frameColor=(0.4, 0.7, 1.0, 1),  # accent-colored thumb/handle
+    thumb_relief=DGG.FLAT,)
+
         self.bodies = [
-            Body(self.loader, self.render, mass=1e11, scale=2, colour=(0.6, 0.6, 1.0, 1.0), position=(randint(0, 30), randint(0, 30), randint(0, 30))),
-            Body(self.loader, self.render, mass=1e11, scale=2, colour=(0.1, 0.8, 1.0, 1.0), position=(randint(0, 30), randint(0, 30), randint(0, 30))),
-            Body(self.loader, self.render, mass=1e11, scale=2, colour=(1.0, 0.0, 1.0, 1.0), position=(randint(0, 30), randint(0, 30), randint(0, 30))),
-            Body(self.loader, self.render, mass=1e11, scale=2, colour=(1.0, 0.0, 1.0, 1.0), position=(randint(0, 30), randint(0, 30), randint(0, 30))),
-            Body(self.loader, self.render, mass=1e11, scale=2, colour=(1.0, 0.0, 1.0, 1.0), position=(randint(0, 30), randint(0, 30), randint(0, 30))),
-            Body(self.loader, self.render, mass=1e11, scale=2, colour=(1.0, 0.0, 1.0, 1.0), position=(randint(0, 30), randint(0, 30), randint(0, 30)))
+
+        Body(self.loader, self.render, mass=1e14, scale=3, colour=(1.0, 0.9, 0.6, 1.0), position=(0, 0, 0), velocity=Vec3(0, 0, 0)),
+        Body(self.loader, self.render, mass=1e5, scale=1, colour=(0.3, 0.6, 1.0, 1.0), position=(10, 0, 0), velocity=Vec3(0, 0, 25.83))
         ]
 
         self.taskMgr.add(self.update, "update")
 
     def update(self, task):
-        dt = globalClock.getDt()
+        dt = globalClock.getDt()*self.slider['value']
         forces = []
         for body in self.bodies:
             F=Vec3(0,0,0)
             for other in self.bodies:
                 if body is not other:
                     F+=Body.force(body, other)
+#                    Body.is_colliding(body, other)
             forces.append(F)
         for body, F in zip(self.bodies, forces):
             Body.applyForce(body, F, dt)
+        for i in range(len(self.bodies)):
+            for j in range(i+1, len(self.bodies)):
+                if Body.is_colliding(self.bodies[i], self.bodies[j]):
+                    Body.apply_collision(self.bodies[i], self.bodies[j])
+            
 
 #        diff=-self.sphere.getPos()+self.model.getPos()
 #        self.r= diff.length()
